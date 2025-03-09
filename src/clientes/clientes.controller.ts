dece660435d1f8d46aafc3719075bcd2422d7d3c
Rafael <rafaelportot@proton.me>
2025-03-09 14:14:17 -0300

    criado controller de Clientes

--- src/clientes/clientes.controller.ts
+++ src/clientes/clientes.controller.ts
@@ -1,34 +1,45 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { ClientesService } from './clientes.service';
-import { CreateClienteDto } from './dto/create-cliente.dto';
-import { UpdateClienteDto } from './dto/update-cliente.dto';
+import { Prisma } from '@prisma/client';
 
-@Controller('clientes')
+@Controller('clientes') // Define a rota base "/clientes"
 export class ClientesController {
   constructor(private readonly clientesService: ClientesService) {}
 
   @Post()
-  create(@Body() createClienteDto: CreateClienteDto) {
-    return this.clientesService.create(createClienteDto);
+  async create(@Body() data: Prisma.ClientesCreateInput) {
+    return this.clientesService.create(data);
   }
 
   @Get()
-  findAll() {
-    return this.clientesService.findAll();
+  async findAll(
+    @Query('skip') skip?: number,
+    @Query('take') take?: number,
+    @Query('cursor') cursor?: string,
+    @Query('where') where?: string,
+    @Query('orderBy') orderBy?: string,
+  ) {
+    return this.clientesService.findAll({
+      skip: skip ? Number(skip) : undefined,
+      take: take ? Number(take) : undefined,
+      cursor: cursor ? { id: Number(cursor) } : undefined,
+      where: where ? JSON.parse(where) : undefined,
+      orderBy: orderBy ? JSON.parse(orderBy) : undefined,
+    });
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.clientesService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    return this.clientesService.findOne({ id: Number(id) });
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateClienteDto: UpdateClienteDto) {
-    return this.clientesService.update(+id, updateClienteDto);
+  async update(@Param('id') id: string, @Body() data: Prisma.ClientesUpdateInput) {
+    return this.clientesService.update({ where: { id: Number(id) }, data });
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.clientesService.remove(+id);
+  async remove(@Param('id') id: string) {
+    return this.clientesService.remove({ id: Number(id) });
   }
 }
